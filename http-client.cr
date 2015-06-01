850594806e8e375aa84a83a53858b7419a0a35c0
rilian <dmitriyis@gmail.com>
2015-06-01 23:03:29 +0300

    remove output from http client/server to try 1M

--- http-client.cr
+++ http-client.cr
@@ -2,9 +2,11 @@ require "http"
 
 client = HTTP::Client.new("0.0.0.0", 8080)
 
-1000.times do |i|
-  puts "[#{Time.now}] Sending #{i} request"
-  response = client.get("/")
-  puts "[#{Time.now}] Got response #{response.body}"
+1_000_000.times do |i|
+  # puts "[#{Time.now}] Sending #{i} request"
+  # response = 
+  client.get("/")
+  # puts "[#{Time.now}] Got response #{response.body}"
 end
 
+puts "Done 1M requests"
--- http-server.cr
+++ http-server.cr
@@ -1,7 +1,7 @@
 require "http/server"
 
 server = HTTP::Server.new(8080) do |request|
-  puts "[#{Time.now}] Received request #{request}"
+  #puts "[#{Time.now}] Received request #{request}"
   HTTP::Response.ok "text/plain", "Hello world! The time is #{Time.now}"
 end
 
